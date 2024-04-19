0598af0368326ee1c8ed89f93969d75727e45419
Anita Caron <anitacaron@users.noreply.github.com>
2024-04-19 14:37:21 +0100

    add citation to `uns`

--- src/scxa2cxg.py
+++ src/scxa2cxg.py
@@ -120,6 +120,7 @@ def convert(study: str):
     new_uns = ann_data.uns.copy()
     new_uns["title"] = metadata.loc[["Investigation Title"]]["1.1"].values[0]
     new_uns["default_embedding"] = "X_umap_neighbors_n_neighbors_20"
+    new_uns["citation"] = f"Publication: https://doi.org/{metadata.loc[['Publication DOI']]['1.1'].values[0]}"
 
     ann_data.obs = new_obs
     ann_data.var = new_var
